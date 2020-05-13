d16dc032ceef1e0cf3a5360d184ff408f1d3c1b4
rashrayhan <rashrayhan@gmail.com>
2020-05-13 12:27:51 -0500

    vue skeleton  configuration

---
+++ client/src/services/VideoService.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const url = 'http://localhost:4600/api/videos/';
+
+class VideoService{
+  //Get Videos
+  static getVideos(){
+    return new Promise(async(resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map(videos => ({
+            ...videos
+          }))
+        );
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  //Get Video by ID
+
+}
+
+export default VideoService;
